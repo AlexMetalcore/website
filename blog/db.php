0cb675a5536f76c0c6556874d07eacfe30ebbb67
Alex <a.alexandrov@smart-city.com.ua>
2017-04-25 02:01:52 +0300

    Update db.php

--- blog/db.php
+++ blog/db.php
@@ -1,25 +1,7 @@
 <?php
-/*define('MYSQL_SERVER' , 'localhost');
-define('MYSQL_USER' , 'blog');
-define('MYSQL_PASSWORD' , '623apekru');
-define('MYSQL_DB' , 'blog');
-
-$host = "localhost";
-$user = "blog";
-$pass = "623apekru";
-$db = "blog";
-
-function db_connect () {
-    $connect_db = new mysqli($host, $user , $pass , $db);
-    $connect_db->set_charset("utf8");
-        if ($connect_db->connect_error){
-                die ("Ошибка соединения с базой:" . $connect_db->connect_error);
-       }
-    return $connect_db;
-}*/
 define('MYSQL_SERVER' , 'localhost');
 define('MYSQL_USER' , 'blog');
-define('MYSQL_PASSWORD' , '623apekru');
+define('MYSQL_PASSWORD' , 'pass');
 define('MYSQL_DB' , 'blog');
     function db_connect() {
         $connect_db = mysqli_connect(MYSQL_SERVER , MYSQL_USER , MYSQL_PASSWORD , MYSQL_DB) or die ("Error :".mysqli_error($connect_db));
